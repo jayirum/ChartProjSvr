91253cdf84ccc8852e7eb25ccb35b3a00a2b99b5
jayirum <jay.bwkim@gmail.com>
2019-01-02 22:36:40 +0900

    CSharp TCP client outsourcing source

---
+++ Source/CSharp/TCPClient_Outsourcing/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPacketParser : Form
+    {
+        private PPTcpClient tcpClient;
+
+        public frmPacketParser()
+        {
+            InitializeComponent();
+
+            txtIP.Text = "127.0.0.1";
+            txtPort.Text = "8188";
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+
+            Color c = Color.Red;
+            PPTcpClient.ConnectionStatus status = PPTcpClient.ConnectionStatus.DISCONNECTED;
+            string btnText = "Connect";
+            bool sendEnabled = false;
+            if (tcpClient != null)
+            {
+                status = tcpClient.Status;
+            }
+            if (status == PPTcpClient.ConnectionStatus.CONNECTED)
+            {
+                btnText = "Disconnect";
+                c = Color.Green;
+                sendEnabled = true;
+            } else if (status == PPTcpClient.ConnectionStatus.CONNECTING)
+            {
+                btnText = "Disconnect";
+                c = Color.Gray;
+            } else
+            {
+                btnText = "Connect";
+                c = Color.Red;
+            }
+            if (txtStatus.InvokeRequired)
+            {
+                txtStatus.Invoke((MethodInvoker)delegate {
+                    txtStatus.Text = status.ToString();
+                    txtStatus.BackColor = c;
+                    btnConnect.Text = btnText;
+                    txtSend.Enabled = sendEnabled;
+                    btnSend.Enabled = sendEnabled;
+                    });
+
+            } else {
+                txtStatus.Text = status.ToString();
+                txtStatus.BackColor = c;
+                btnConnect.Text = btnText;
+                txtSend.Enabled = sendEnabled;
+                btnSend.Enabled = sendEnabled;
+            }
+
+        }
+        private bool CheckIP(String ip)
+        {
+            bool ret = true;
+            Regex reipv4 = new Regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+
+            ret = reipv4.IsMatch(ip);
+            return ret;
+
+        }
+        private bool CheckPort(String portstr)
+        {
+            bool ret = true;
+            int port;
+            if (!Int32.TryParse(portstr, out port))
+            {
+                ret = false;
+            } else
+            {
+                if(port <= 0 || port >= 65535)
+                {
+                    ret = false;
+                }
+            }
+            return ret;
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            if(tcpClient == null)
+            {
+                String ip = txtIP.Text.Trim();
+                String portstr = txtPort.Text.Trim();
+                int port = 0;
+                if(!CheckIP(ip))
+                {
+                    MessageBox.Show("Invalid IPv4 Address");
+                    txtIP.Focus();
+                    return;
+                }
+                if(!CheckPort(portstr))
+                {
+                    MessageBox.Show("Invalid Port");
+                    txtPort.Focus();
+                    return;
+                } else
+                {
+                    Int32.TryParse(portstr, out port);
+                }
+                tcpClient = new PPTcpClient(ip, port);
+                tcpClient.StatusUpdated += TcpClient_StatusUpdated;
+                tcpClient.onPacket += TcpClient_onPacket;
+                tcpClient.StartConnection();
+            } else
+            {
+
+                if(tcpClient.Status == PPTcpClient.ConnectionStatus.CONNECTED 
+                    || tcpClient.Status == PPTcpClient.ConnectionStatus.CONNECTING)
+                {
+                    tcpClient.StopConnection();
+                    tcpClient.StatusUpdated -= TcpClient_StatusUpdated;
+                    tcpClient.onPacket -= TcpClient_onPacket;
+                    tcpClient = null;
+
+                }
+            }
+            UpdateUI();
+            
+        }
+
+        private void TcpClient_onPacket(byte[] data)
+        {
+            // call PacketParser
+
+            Console.WriteLine(String.Format("{0} onPacket", DateTime.Now.ToString("mm:ss.zzz")));
+            txtLog.Invoke((MethodInvoker)delegate {
+                Console.WriteLine(String.Format("{0} Write Log", DateTime.Now.ToString("mm:ss.zzz")));
+                txtLog.AppendText(String.Format("GetOnePacket - {0}\n", Encoding.ASCII.GetString(data)));
+            });
+        }
+
+        private void TcpClient_StatusUpdated()
+        {
+            // Console.WriteLine(String.Format("Tcp Client Status Updated {0}", this.tcpClient.Status));
+            String newstatus = this.tcpClient.Status.ToString();
+            txtLog.Invoke((MethodInvoker)delegate
+            {
+                txtLog.AppendText(String.Format("Tcp Client {0}\n", newstatus));
+                
+            });
+            UpdateUI();
+            if(tcpClient.Status == PPTcpClient.ConnectionStatus.DISCONNECTED)
+            {
+                tcpClient.StopConnection();
+                tcpClient.StatusUpdated -= TcpClient_StatusUpdated;
+                tcpClient.onPacket -= TcpClient_onPacket;
+                tcpClient = null;
+            }
+            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            String s = txtSend.Text;
+            if (s.Length <= 0) return;
+            if(tcpClient != null && tcpClient.Connected)
+            {
+                int buflength = s.Length + 6;
+                byte[] buf = new byte[buflength];
+
+                int i = 0;
+                buf[i] = 0x02;
+                i += 1;
+
+                string lengthstr = buflength.ToString("D4");
+                Encoding.ASCII.GetBytes(lengthstr).CopyTo(buf, i);
+                i += 4;
+                Encoding.ASCII.GetBytes(s).CopyTo(buf, i);
+                i += s.Length;
+                buf[i] = 0x03;
+                i += 1;
+
+                int sent = tcpClient.SendData(buf);
+                if(sent < buflength)
+                {
+                    // error
+                } else
+                {
+                    txtLog.AppendText(String.Format("Sent {0} Bytes\n", sent));
+                }
+            }
+        }
+
+        private void frmPacketParser_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if(tcpClient != null)
+            {
+                tcpClient.StopConnection();
+                tcpClient.StatusUpdated -= TcpClient_StatusUpdated;
+                tcpClient.onPacket -= TcpClient_onPacket;
+                tcpClient = null;
+            }
+
+        }
+    }
+}
---
+++ Source/CSharp/TCPClient_Outsourcing/WindowsFormsApplication1/PPTcpClient.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    internal class PPTcpClient
+    {
+        private Socket tcpSocket;
+        private string host;
+        private int port;
+        private Thread networkThread;
+        private Thread parserThread;
+
+        private const int PACKET_SIZE = 8192;
+        private bool Quit
+        {
+            get; set;
+        }
+        private PacketParser packetParser;
+
+        AutoResetEvent waitHandle = new AutoResetEvent(false);
+
+        public enum ConnectionStatus 
+        {
+            DISCONNECTED,
+            CONNECTING,
+            CONNECTED
+        }
+
+        public ConnectionStatus Status
+        {
+            get;
+            set;
+        }
+
+        public bool Connected
+        {
+            get
+            {
+                return (Status == ConnectionStatus.CONNECTED
+                    && (tcpSocket != null)
+                    && !(tcpSocket.Poll(1, SelectMode.SelectRead)
+                        && tcpSocket.Available == 0)
+                    );
+            }
+        }
+        public delegate void StatusUpdateHandler ();
+        public event StatusUpdateHandler StatusUpdated;
+        public delegate void OnPacketHandler (byte[] data);
+        public event OnPacketHandler onPacket;
+
+        public PPTcpClient(string host, int port)
+        {
+            this.host = host;
+            this.port = port;
+
+            packetParser = new PacketParser();
+        }
+
+        ~PPTcpClient()
+        {
+            Quit = true;
+        }
+
+        public void StartConnection()
+        {
+            ThreadStart networkStart = new ThreadStart(NetworkThread);
+            ThreadStart parserStart = new ThreadStart(ParserThread);
+            networkThread = new Thread(networkStart);
+            networkThread.Start();
+            parserThread = new Thread(parserStart);
+            parserThread.Start();
+        }
+
+        public void StopConnection()
+        {
+            Quit = true;
+            if(tcpSocket != null)
+            {
+                tcpSocket.Close();
+                tcpSocket = null;
+            }
+            if (networkThread != null)
+            {
+                networkThread.Abort();
+            }
+            if (parserThread != null)
+            {
+                waitHandle.Set();
+                parserThread.Abort();
+            }
+        }
+
+        private void NetworkThread()
+        {
+            tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.ReceiveBufferSize = PACKET_SIZE;
+            tcpSocket.SendBufferSize = PACKET_SIZE;
+            tcpSocket.NoDelay = true;
+            tcpSocket.DontFragment = true;
+            // tcpSocket.ReceiveTimeout = 5000;
+            // tcpSocket.SendTimeout = 5000;
+
+            while (!tcpSocket.Connected && !Quit) {
+                // TODO raise connecting event
+                UpdateStatus(ConnectionStatus.CONNECTING);
+                try
+                {
+                    tcpSocket.Connect(this.host, this.port);
+                } catch(SocketException e)
+                {
+                    Console.WriteLine("Error connecting to server {0} {1}", e.ErrorCode, e.Message);
+                }
+
+                if (tcpSocket.Connected)
+                {
+                    // raise connected event
+                    UpdateStatus(ConnectionStatus.CONNECTED);
+                }
+            }
+
+            while(!Quit && Connected)
+            {
+                byte[] buffer = new byte[PACKET_SIZE];
+                try
+                {
+                    int r = tcpSocket.Receive(buffer);
+                    if (r > 0)
+                    {
+                        // push packet
+                        Console.WriteLine(String.Format("{0} Receive", DateTime.Now.ToString("mm:ss.fff")));
+                        packetParser.AddPacket(buffer, r);
+                        waitHandle.Set();
+                    }
+                } catch(SocketException e)
+                {
+
+                    Console.WriteLine("Error receiving from socket {0} {1}", e.ErrorCode, e.Message);
+
+                }
+            }
+
+            Quit = true;
+            UpdateStatus(ConnectionStatus.DISCONNECTED);
+        }
+
+        public int SendData(byte[] data)
+        {
+            int s = 0;
+            if(tcpSocket != null && tcpSocket.Connected)
+            {
+                s = tcpSocket.Send(data);
+            }
+
+            return s;
+        }
+
+        private void UpdateStatus(ConnectionStatus status)
+        {
+
+            ConnectionStatus ostatus = this.Status;
+            this.Status = status;
+
+            if(ostatus != status && StatusUpdated != null)
+            {
+                StatusUpdated();
+            }
+        }
+
+        private void ParserThread()
+        {
+
+            while(!Quit)
+            {
+                waitHandle.WaitOne();
+
+                while (packetParser.MorePacket())
+                {
+                    byte[] msg = packetParser.GetOnePacket();
+                    if (msg != null)
+                    {
+
+                        if (onPacket != null)
+                        {
+                            onPacket(msg);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
