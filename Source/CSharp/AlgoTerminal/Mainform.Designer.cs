71a4a8b041f618d5aea2ba65864529f7904c541d
jayirum <jay.bwkim@gmail.com>
2019-01-02 22:37:17 +0900

    Algo Terminal 생성

---
+++ Source/CSharp/AlgoTerminal/Mainform.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AlgoTerminal
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.hHippoChart1 = new Hippo.WindowsForm4.hHippoChart();
+            this.pnlDF = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Close = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ask = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnlChart = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCnfg = new System.Windows.Forms.Button();
+            this.pnlDF.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlChart.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hHippoChart1
+            // 
+            this.hHippoChart1.BackColor = System.Drawing.Color.Transparent;
+            this.hHippoChart1.ChartGraphic = null;
+            this.hHippoChart1.ChartID = null;
+            this.hHippoChart1.ChartImage = null;
+            this.hHippoChart1.ChartRectangle = ((System.Drawing.RectangleF)(resources.GetObject("hHippoChart1.ChartRectangle")));
+            this.hHippoChart1.Designer = ((Hippo.ChartDesigner)(resources.GetObject("hHippoChart1.Designer")));
+            this.hHippoChart1.DesignType = Hippo.ChartDesignType.None;
+            this.hHippoChart1.Direction = Hippo.GraphAreaLocation.Vertical;
+            this.hHippoChart1.Height2 = 270F;
+            this.hHippoChart1.IsDrag = false;
+            this.hHippoChart1.IsEmptyanalysis = true;
+            this.hHippoChart1.IsUseContextMenu = true;
+            this.hHippoChart1.Layout = ((Hippo.Layout)(resources.GetObject("hHippoChart1.Layout")));
+            this.hHippoChart1.Left = 0F;
+            this.hHippoChart1.LegendBox = ((Hippo.LegendBox)(resources.GetObject("hHippoChart1.LegendBox")));
+            this.hHippoChart1.Location = new System.Drawing.Point(3, 3);
+            this.hHippoChart1.Logo = ((Hippo.Logo)(resources.GetObject("hHippoChart1.Logo")));
+            this.hHippoChart1.MinimumSize = new System.Drawing.Size(200, 140);
+            this.hHippoChart1.Name = "hHippoChart1";
+            this.hHippoChart1.PaletteType = Hippo.PaletteType.Default;
+            this.hHippoChart1.ProcessorType = "64";
+            this.hHippoChart1.SeriesAreaRate = null;
+            this.hHippoChart1.SeriesListDictionary = ((Hippo.SeriesListDictionary)(resources.GetObject("hHippoChart1.SeriesListDictionary")));
+            this.hHippoChart1.Size = new System.Drawing.Size(428, 319);
+            this.hHippoChart1.TabIndex = 0;
+            this.hHippoChart1.Titles = ((Hippo.Title)(resources.GetObject("hHippoChart1.Titles")));
+            this.hHippoChart1.Top = 0F;
+            this.hHippoChart1.Width2 = 400F;
+            this.hHippoChart1.ChartSizeChanged += new System.EventHandler(this.hHippoChart1_ChartSizeChanged);
+            // 
+            // pnlDF
+            // 
+            this.pnlDF.Controls.Add(this.dataGridView1);
+            this.pnlDF.Location = new System.Drawing.Point(4, 12);
+            this.pnlDF.Name = "pnlDF";
+            this.pnlDF.Size = new System.Drawing.Size(200, 325);
+            this.pnlDF.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Close,
+            this.Bid,
+            this.Ask});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Close
+            // 
+            this.Close.HeaderText = "Close";
+            this.Close.Name = "Close";
+            this.Close.ReadOnly = true;
+            this.Close.Width = 50;
+            // 
+            // Bid
+            // 
+            this.Bid.HeaderText = "Bid";
+            this.Bid.Name = "Bid";
+            this.Bid.ReadOnly = true;
+            this.Bid.Width = 50;
+            // 
+            // Ask
+            // 
+            this.Ask.HeaderText = "Ask";
+            this.Ask.Name = "Ask";
+            this.Ask.ReadOnly = true;
+            this.Ask.Width = 50;
+            // 
+            // pnlChart
+            // 
+            this.pnlChart.Controls.Add(this.hHippoChart1);
+            this.pnlChart.Location = new System.Drawing.Point(221, 12);
+            this.pnlChart.Name = "pnlChart";
+            this.pnlChart.Size = new System.Drawing.Size(434, 325);
+            this.pnlChart.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(661, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(87, 28);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(5, 344);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(741, 167);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(730, 162);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnCnfg
+            // 
+            this.btnCnfg.Location = new System.Drawing.Point(661, 42);
+            this.btnCnfg.Name = "btnCnfg";
+            this.btnCnfg.Size = new System.Drawing.Size(87, 28);
+            this.btnCnfg.TabIndex = 5;
+            this.btnCnfg.Text = "Get Cnfg";
+            this.btnCnfg.UseVisualStyleBackColor = true;
+            this.btnCnfg.Click += new System.EventHandler(this.btnCnfg_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 519);
+            this.Controls.Add(this.btnCnfg);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pnlChart);
+            this.Controls.Add(this.pnlDF);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.pnlDF.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlChart.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Hippo.WindowsForm4.hHippoChart hHippoChart1;
+        private System.Windows.Forms.Panel pnlDF;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Close;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ask;
+        private System.Windows.Forms.Panel pnlChart;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCnfg;
+    }
+}
+
---
+++ Source/CSharp/AlgoTerminal/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Utils
+{
+    public class CUtils
+    {
+        //////////////////////////////////////////////////////////////////////
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section,
+            string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section,
+                 string key, string def, StringBuilder retVal,
+            int size, string filePath);
+        //////////////////////////////////////////////////////////////////////
+
+        public static string GetConfig(string sCnfgFileNm, string sSectionNm, string sKeyNm)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(sSectionNm, sKeyNm, "", temp,255, sCnfgFileNm);
+
+            string result = temp.ToString();
+            int nIdx = result.IndexOf("//");
+            if(nIdx>-1)
+            {
+                result = result.Remove(nIdx, result.Length - nIdx);
+            }
+            return result.Trim(); ;
+        }
+
+        public static string GetCnfgFileNm(string sDir, string sFileNm)
+        {
+            string sFullFileName = sDir;
+            if (sDir[sDir.Length - 1] != '\\')
+                sFullFileName += '\\';
+
+            // remove .exe
+            sFileNm = sFileNm.ToUpper();
+            int idx = sFileNm.IndexOf(".EXE");
+            if(idx>-1)
+            {
+                sFileNm = sFileNm.Remove(idx, sFileNm.Length - idx);
+            }
+
+            sFullFileName += sFileNm + ".ini";
+            return sFullFileName;
+        }
+    }
+}
